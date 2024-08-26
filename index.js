669324c447013f9498b0caac7a595ae5d8b258fd
Dhruv Atreja <dhruvatrejamain@gmail.com>
2024-08-27 02:15:59 +0530

    sqlite server

--- index.js
+++ index.js
@@ -3,24 +3,6 @@ const mongoose = require("mongoose");
 const projectRouter = require("./routes/ProjectRoutes.js");
 
 const app = express();
-app.use(express.json()); // Make sure it comes back as json
-
-//TODO - Replace you Connection String here
-// mongoose
-//   .connect(
-//     "mongodb+srv://dhruvatreja:dhruvshubhrA1@cluster0.4a8oloa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//   )
-//   .then((success) => {
-//     console.log("Success Mongodb connection");
-//   })
-//   .catch((err) => {
-//     console.log("Error Mongodb connection");
-//     console.log(err);
-//   });
 
 app.use(projectRouter);
 
